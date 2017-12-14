52c2a5915edfbf4730287fc8c60a9e744a179f22
Steven Pickles <Steven Pickles@localhost>
2017-12-14 10:23:06 -0500

    Add preliminary support for getting a recursive path list from a base path.
    Add GetRecursivePathList special case support for when a directory is the base
    path and the directory does not exist.

---
+++ DirectoryComparer.Tests/PathList.Tests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DirectoryComparer.Tests
+{
+    [TestClass]
+    public class PathListTests
+    {
+        [TestMethod]
+        public void Test_PathList_WhenGettingPathListOfNonexistingDirectory_ThenEmptyListIsReturned()
+        {
+            List<string> pathList = PathList.GetRecursivePathList( "directoryDoesNotExist" );
+            Assert.AreEqual( 0 , pathList.Count );
+        }
+    }
+}
---
+++ DirectoryComparer/PathList.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace DirectoryComparer
+{
+    public static class PathList
+    {
+        public static List<string> GetRecursivePathList( string basePath )
+        {
+            /* special case -- basePath is a nonexisting directory */
+            if ( !Directory.Exists( basePath ) )
+            {
+                return new List<string>();
+            }
+
+            return new List<string>();
+        }
+    }
+}
