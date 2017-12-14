7bc7d5845564c371e09435f454d4dfdb710f9e27
Steven Pickles <Steven Pickles@localhost>
2017-12-14 15:58:29 -0500

    Add support for recursively getting relative path for files and directories.

--- DirectoryComparer.Tests/PathList.Tests.cs
+++ DirectoryComparer.Tests/PathList.Tests.cs
@@ -7,11 +7,27 @@ namespace DirectoryComparer.Tests
     [TestClass]
     public class PathListTests
     {
+        protected const string rootPath = ".\\root\\";
+        protected const string basePath = rootPath + "base\\";
+
         [TestMethod]
         public void Test_PathList_WhenGettingPathListOfNonexistingDirectory_ThenEmptyListIsReturned()
         {
-            List<string> pathList = PathList.GetRecursivePathList( "directoryDoesNotExist" );
+            List<string> pathList = PathList.GetRecursivePathList( basePath + "directoryDoesNotExist\\" );
             Assert.AreEqual( 0 , pathList.Count );
+        }
+
+        [TestMethod]
+        public void Test_PathList_WhenGettingPathListOfDirectory_ThenPathListIsReturned()
+        {
+            List<string> pathList = PathList.GetRecursivePathList( basePath );
+            Assert.AreEqual( 6 , pathList.Count );
+            Assert.AreEqual( "hello_world.txt" , pathList[ 0 ] );
+            Assert.AreEqual( "key.dat" , pathList[ 1 ] );
+            Assert.AreEqual( "some_folder\\" , pathList[ 2 ] );
+            Assert.AreEqual( "some_folder\\some_file.txt" , pathList[ 3 ] );
+            Assert.AreEqual( "some_folder\\nested_folder\\" , pathList[ 4 ] );
+            Assert.AreEqual( "some_folder\\nested_folder\\leaf.txt" , pathList[ 5 ] );
         }
     }
 }
--- DirectoryComparer/PathList.cs
+++ DirectoryComparer/PathList.cs
@@ -6,15 +6,75 @@ namespace DirectoryComparer
 {
     public static class PathList
     {
-        public static List<string> GetRecursivePathList( string basePath )
+        /// <summary>
+        ///     Gets recursive path list with paths prefixed paths.
+        /// </summary>
+        ///
+        /// <param name="path">
+        ///     The starting path.
+        ///     </param>
+        ///
+        /// <returns>
+        ///     The recursive path list.
+        /// </returns>
+        public static List<string> GetRecursivePathList( string path )
         {
-            /* special case -- basePath is a nonexisting directory */
-            if ( !Directory.Exists( basePath ) )
+            return GetRecursivePathList( path , "" );
+        }
+
+
+        /// <summary>
+        ///     Gets recursive path list with paths prefixed paths.
+        /// </summary>
+        ///
+        /// <param name="path">
+        ///     The starting path.
+        ///     </param>
+        /// <param name="prefix">
+        ///     The prefix to prepend filenames and subdirectories.
+        ///     </param>
+        ///
+        /// <returns>
+        ///     The recursive path list.
+        /// </returns>
+        private static List<string> GetRecursivePathList( string path , string prefix )
+        {
+            /* special case -- basePath is a nonexistant directory */
+            if ( !Directory.Exists( path ) )
             {
                 return new List<string>();
             }
 
-            return new List<string>();
+            List<string> pathList = new List<string>();
+
+            /* add all local directory filenames to the path list */
+            List<string> filenameList = new List<string>( Directory.GetFiles( path ) );
+            foreach ( string filename in filenameList )
+            {
+                string filenameWithPrefix = prefix + Path.GetFileName( filename );
+
+                pathList.Add( filenameWithPrefix );
+            }
+
+            /* recursively add all subdirectories (and subdirectory filenames) to the path list */
+            List<string> subdirectoryList = new List<string>( Directory.GetDirectories( path ) );
+            foreach ( string subdirectory in subdirectoryList )
+            {
+                /* generate the subdirectory name with the prefix */
+                string subdirectoryFullPath = Path.GetFullPath( subdirectory );
+                string subdirectoryFullPathAsFilename = subdirectoryFullPath.TrimEnd( Path.DirectorySeparatorChar );
+                string subdirectoryName = Path.GetFileName( subdirectoryFullPath );
+                string subdirectoryNameWithPrefix = prefix + subdirectoryName + Path.DirectorySeparatorChar;
+
+                pathList.Add( subdirectoryNameWithPrefix );
+
+                /* go to any subdirectories */
+                List<string> subdirectoryPathList = PathList.GetRecursivePathList( subdirectory , subdirectoryNameWithPrefix );
+
+                pathList.AddRange( subdirectoryPathList );
+            }
+
+            return pathList;
         }
     }
 }
