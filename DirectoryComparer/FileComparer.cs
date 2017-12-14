eec27a8b2972b550f4a9d8d1da939c6c3d0eba2e
Steven Pickles <Steven Pickles@localhost>
2017-12-14 17:16:36 -0500

    Add preliminary support for file comparer.

---
+++ DirectoryComparer/FileComparer.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+
+namespace DirectoryComparer
+{
+    public class FileComparer
+    {
+        public bool IsDifferent
+        {
+            get
+            {
+                if ( IsSizeMismatch ) { return true; }
+                if ( IsHashMismatch ) { return true; }
+                if ( IsLocationMismatch ) { return true; }
+
+                return false;
+            }
+        }
+
+
+        public bool IsSizeMismatch
+        {
+            get
+            {
+                return ( _a.Length != _b.Length );
+            }
+        }
+
+
+        public bool IsHashMismatch
+        {
+            get
+            {
+                return ( _hashA.HashString != _hashB.HashString );
+            }
+        }
+
+
+        public bool IsLocationMismatch
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        protected FileInfo _a;
+        protected FileInfo _b;
+
+        protected FileHash _hashA;
+        protected FileHash _hashB;
+
+
+        public FileComparer( string a , string b )
+        {
+            _a = new FileInfo( a );
+            _b = new FileInfo( b );
+
+            _hashA = new FileHash( a );
+            _hashB = new FileHash( b );
+        }
+    }
+}
